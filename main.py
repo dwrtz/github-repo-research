1a64dfde55e22a30533f7d7ebc8256ade6204f32
David Wurtz <davidjwurtz@gmail.com>
2023-05-24 19:37:08 -0700

    get closed PRs for each repo

--- main.py
+++ main.py
@@ -12,15 +12,32 @@ g = Github(gat)
 search_query = "stars:100..10000 language:python"
 result = g.search_repositories(query=search_query, sort="stars", order="desc")
 
-PAGE_SIZE = 4
-PAGE_NUMBER = 1
+# repo pagination
+REPOS_PAGE_SIZE = 4
+REPOS_PAGE_NUMBER = 1
 
-for i, repo in enumerate(
-    result[PAGE_SIZE * (PAGE_NUMBER - 1) : PAGE_SIZE * PAGE_NUMBER]
-):
-    index = PAGE_SIZE * (PAGE_NUMBER - 1) + i + 1
+# pr pagination
+PR_PAGE_SIZE = 4
+PR_PAGE_NUMBER = 1
+
+start_index = REPOS_PAGE_SIZE * (REPOS_PAGE_NUMBER - 1)
+end_index = REPOS_PAGE_SIZE * REPOS_PAGE_NUMBER
+for i, repo in enumerate(result[start_index:end_index]):
+    index = REPOS_PAGE_SIZE * (REPOS_PAGE_NUMBER - 1) + i + 1
     print(f"Repo #{index} owner: {repo.owner.login}")
     print(f"Repo #{index} name: {repo.name}")
     print(f"Repo #{index} stars: {repo.stargazers_count}")
     print(f"Repo #{index} url: {repo.html_url}")
-    print("")
+
+    # get recent closed pull requests
+    pulls = repo.get_pulls(state="closed", sort="created", direction="desc")
+    start_index = PR_PAGE_SIZE * (PR_PAGE_NUMBER - 1)
+    end_index = PR_PAGE_SIZE * PR_PAGE_NUMBER
+    for i, pr in enumerate(pulls[start_index:end_index]):
+        index = PR_PAGE_SIZE * (PR_PAGE_NUMBER - 1) + i + 1
+        print(f"\tPR #{index} title: {pr.title}")
+        print(f"\tPR #{index} created at: {pr.created_at}")
+        print(f"\tPR #{index} merged at: {pr.merged_at}")
+        print(f"\tPR #{index} url: {pr.html_url}")
+        print(f"\tPR #{index} diff url: {pr.diff_url}")
+        print("")
